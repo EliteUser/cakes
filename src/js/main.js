6805f0367181fe2e817d947c77ed058b78dc1aa9
Andrey <unknownuser017@gmail.com>
2018-07-17 15:01:33 +0300

    style order

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 var navButton = document.querySelector(".main-navigation__button");
 var navList = document.querySelector(".main-navigation__list");
 
+document.querySelector("html").classList.remove("nojs");
+
 navButton.addEventListener("click", function () {
   navButton.classList.toggle("main-navigation__button--active");
   navList.classList.toggle("main-navigation__list--active");
